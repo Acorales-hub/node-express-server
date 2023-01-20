0b0673aa45006583874d8d37ee208129133293a2
Asdrubal Corales <skorpico@gmail.com>
2023-01-20 12:20:00 -0400

    Se ha Copiado node-express-server.js al directorio node-express-server

---
+++ node-express-server.js
@@ -0,0 +1,15 @@
+/*.::CORALESoftware::.
+Name of the proyect: 𝙉𝙤𝙙𝙚 𝙀𝙭𝙥𝙧𝙚𝙨𝙨 𝙎𝙚𝙧𝙫𝙚𝙧
+Made by: Asdrúbal José Corales Pérez
+Description: This is a project made in Node.Js, for my briefcase
+License: undifined.
+Contac me in: asdrubalcorales92@gmail.com
+My phone number is: +58 412 4811678*/
+console.log('\n==================𝙉𝙤𝙙𝙚 𝙀𝙭𝙥𝙧𝙚𝙨𝙨 𝙎𝙚𝙧𝙫𝙚𝙧=========== ')
+console.log('                    ░░░░░░░░▄▄█▄▄')
+console.log('By Ing. A Corales P.░░░░▀▀▀██▀▀▀██▀▀▀')
+console.log('                    ▄▄▄▄▄▄▄███████▄▄▄▄▄▄▄')
+console.log('                    ░░█▄█░░▀██▄██▀░░█▄█')
+console.log('================================================== ')
+
+console.log('\n Hola mundo');
