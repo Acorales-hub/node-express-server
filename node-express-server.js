728140454347a2ff80eb566ba6009f06c2db8471
Asdrubal Corales <skorpico@gmail.com>
2023-01-20 23:35:00 -0400

    Se agrego la función que inicia Node Express Server así como comentarios en diferentes líneas del código

--- node-express-server.js
+++ node-express-server.js
@@ -8,8 +8,22 @@ My phone number is: +58 412 4811678*/
 console.log('\n==================𝙉𝙤𝙙𝙚 𝙀𝙭𝙥𝙧𝙚𝙨𝙨 𝙎𝙚𝙧𝙫𝙚𝙧=========== ')
 console.log('                    ░░░░░░░░▄▄█▄▄')
 console.log('By Ing. A Corales P.░░░░▀▀▀██▀▀▀██▀▀▀')
-console.log('                    ▄▄▄▄▄▄▄███████▄▄▄▄▄▄▄')
+console.log('    MIT LICESE      ▄▄▄▄▄▄▄███████▄▄▄▄▄▄▄')
 console.log('                    ░░█▄█░░▀██▄██▀░░█▄█')
 console.log('================================================== ')
 
-console.log('\n Hola mundo');
+//Vars:
+let serverExpress = require('http'); //serverExpress contiene todos los metodos para hacer funcionar un servidor web a traves del protocolo http
+let codeBrowser = 200;
+let listenPort = 4444;//listen port default  
+
+//Functions:
+
+function startServerExpress(requires,reply)//This function starts Node Express Server
+{
+    reply.writeHead(codeBrowser,{"Content-Type":"text/html"});//whriteHe envía la cabecera al borwserd especifica el tipo de documento a enviar
+    reply.write("<br><center><h1>Welcome to Node Express Server</h1><br>Powered by .::CORALESoftware::.</center>");
+    reply.end(); // This line closes the connection
+}
+
+serverExpress.createServer(startServerExpress).listen(listenPort);
