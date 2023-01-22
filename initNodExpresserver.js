25e193bdcf241ab7d2152d644b5eb96b96e8ab9c
Asdrubal Corales <skorpico@gmail.com>
2023-01-22 16:00:28 -0400

    Se creo initNeExpresserver el cual instancia y inicia el server

---
+++ initNodExpresserver.js
@@ -0,0 +1,12 @@
+/*.::CORALESoftware::.
+Name of the proyect: 𝙉𝙤𝙙𝙚 𝙀𝙭𝙥𝙧𝙚𝙨𝙨 𝙎𝙚𝙧𝙫𝙚𝙧
+Name of the archive: initNodExpresserver
+Made by: Asdrúbal José Corales Pérez
+Description: This is a project made in Node.Js, for my briefcase
+License: MIT.
+Contac me in: asdrubalcorales92@gmail.com
+My phone number is: +58 412 4811678 */
+
+//Instantantiation
+let Server = require('./node-express-server')
+Server.Init();//Call the init
