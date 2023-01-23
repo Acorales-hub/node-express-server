033d3e039e7c70043fc642ff5c6bd2271f381512
Asdrubal Corales <skorpico@gmail.com>
2023-01-23 19:49:18 -0400

    Se creo el archivo enrutador para el servidor

---
+++ routerNodExpresserver.js
@@ -0,0 +1,14 @@
+/*.::CORALESoftware::.
+Name of the proyect: 𝙉𝙤𝙙𝙚 𝙀𝙭𝙥𝙧𝙚𝙨𝙨 𝙎𝙚𝙧𝙫𝙚𝙧
+Name of the archive: routerNodExpresserver
+Made by: Asdrúbal José Corales Pérez
+Description: This is a project made in Node.Js, for my briefcase
+License: MIT.
+Contac me in: asdrubalcorales92@gmail.com
+My phone number is: +58 412 4811678 */
+
+//Functions
+function router()
+{
+    
+}
