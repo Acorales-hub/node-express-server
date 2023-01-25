a8cf32ce7aee5b593d9152cc7065d37963224580
Asdrubal Corales <skorpico@gmail.com>
2023-01-25 11:44:49 -0400

    Se añadieron nuevas líneas de código para genrar color por el terminal de cmdo

--- node-express-server.js
+++ node-express-server.js
@@ -5,18 +5,21 @@ Description: This is a project made in Node.Js, for my briefcase
 License: MIT.
 Contac me in: asdrubalcorales92@gmail.com
 My phone number is: +58 412 4811678 */
-console.log('\n==================𝙉𝙤𝙙𝙚 𝙀𝙭𝙥𝙧𝙚𝙨𝙨 𝙎𝙚𝙧𝙫𝙚𝙧=========== ')
+
+
+//Var:
+let serverExpress = require('http'); //serverExpress contains all the methods of the HTTP protocol to run a web server
+let url = require('url');//Regístra la diercción de URL y permite especificar un comportamiento personalizado 
+const color = require('colors');//Add color 
+let codeBrowser = 200;
+let listenPort = 1111;//listen virtual port default  
+
+console.log('\n==================𝙉𝙤𝙙𝙚 𝙀𝙭𝙥𝙧𝙚𝙨𝙨 𝙎𝙚𝙧𝙫𝙚𝙧===========')
 console.log('                    ░░░░░░░░▄▄█▄▄')
 console.log('By Ing. A Corales P.░░░░▀▀▀██▀▀▀██▀▀▀')
 console.log('    MIT LICESE      ▄▄▄▄▄▄▄███████▄▄▄▄▄▄▄')
 console.log('                    ░░█▄█░░▀██▄██▀░░█▄█')
 console.log('================================================== ')
-
-//Var:
-let serverExpress = require('http'); //serverExpress contains all the methods of the HTTP protocol to run a web server
-let url = require('url');//Regístra la diercción de URL y permite especificar un comportamiento personalizado 
-let codeBrowser = 200;
-let listenPort = 1111;//listen virtual port default  
 
 //Functions:
 function Init(Router,manejador)//Capsule
@@ -25,14 +28,14 @@ function Init(Router,manejador)//Capsule
     {
         //Router:
         var Rute = url.parse(requires.url).pathname;//captures what is entered in the url and stores it in a variable
-        console.log('A new connection detected');
+        console.log('\tA new connection detected'.inverse+'\n');
         Router(manejador,Rute);//Call function router in roterNodExpresserver.js
         reply.writeHead(codeBrowser,{"Content-Type":"text/html"});//whriteHead sends the type of document to the header to the borwser
         reply.write("<br><center><h1>Welcome to Node Express Server</h1><br>Powered by .::CORALESoftware::.</center>");
         reply.write("<br><br>   A new connection detected")
         reply.write("<br><br>   A new rute detected... "+Rute)
         reply.end(); // This line closes the connection
-        console.log('--------------------------------------------------')
+        console.log('--------------------------------------------------'.rainbow)
     }
     serverExpress.createServer(startServerExpress).listen(listenPort);//Call the Node Express Server
 }
