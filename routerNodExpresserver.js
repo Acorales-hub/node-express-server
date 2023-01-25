6f45f489ce14448b231d98c4aa45b16b4511a854
Asdrubal Corales <skorpico@gmail.com>
2023-01-25 11:45:40 -0400

    Se añadieron nuevas líneas de código para generar color por el terminal de cmdo

--- routerNodExpresserver.js
+++ routerNodExpresserver.js
@@ -6,21 +6,20 @@ Description: This is a project made in Node.Js, for my briefcase
 License: MIT.
 Contac me in: asdrubalcorales92@gmail.com
 My phone number is: +58 412 4811678 */
-
 //Var:
-//let route this is the route
+const xcolor= require('colors');
 
 //Functions:
 function Router(manejador,xrute)//this is the router
 {
-    console.log('route detected:'+xrute);
+    console.log('* route detected:'+xrute.yellow.underline);
     
     //Evaluation
     if(typeof manejador[xrute] ==='function')
     {
         return manejador[xrute]();
     }else{
-        console.log("This is not a function: "+xrute);
+        console.log("* This is not a function: "+xrute.red.underline)
     }
     
 }
