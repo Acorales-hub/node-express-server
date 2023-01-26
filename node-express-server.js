eb05d8086a44b604530be25ab884610f2a3bc2e1
Asdrubal Corales <skorpico@gmail.com>
2023-01-26 15:25:33 -0400

    Se agrego código que guarda regístro de las conexiones

--- node-express-server.js
+++ node-express-server.js
@@ -9,6 +9,7 @@ My phone number is: +58 412 4811678 */
 //Var:
 let serverExpress = require('http'); //serverExpress contains all the methods of the HTTP protocol to run a web server
 let url = require('url');//Regístra la diercción de URL y permite especificar un comportamiento personalizado 
+let fs = require('fs');//File system
 const color = require('colors');//Add color 
 let codeBrowser = 200;
 let listenPort = 1111;//listen virtual port default 
@@ -26,9 +27,12 @@ function Init(Router,manejador)//Capsule
     function startServerExpress(requires,reply)//This function starts Node Express Server
     {
         //Router:
-        var Rute = url.parse(requires.url).pathname;//captures what is entered in the url and stores it in a variable
+        let Rute = url.parse(requires.url).pathname;//captures what is entered in the url and stores it in a variable
+        let registration = fs.createWriteStream('registration.txt',{'flags':'a'});//flags. add information registration.txt
         console.log('\tA new connection detected'.inverse+'\n');
         let container = Router(manejador,Rute);//Call function router in roterNodExpresserver.js
+        registration.write(Rute+'\n');
+        
         reply.writeHead(codeBrowser,{"Content-Type":"text/html"});//whriteHead sends the type of document to the header to the borwser
         reply.write("<br><center><h1>Welcome to Node Express Server</h1><br>Powered by .::CORALESoftware::.</center>");
         reply.write("<br><br>   "+Rute+" is a new connection, detected in: "+container)
