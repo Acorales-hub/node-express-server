6f39091643862c77da215cd6e64f8b514f831152
Asdrubal Corales <skorpico@gmail.com>
2023-01-28 14:10:51 -0400

    Se modifico lasvariables aplicando Hoisting y se añadio una línea para leer archivos html

--- node-express-server.js
+++ node-express-server.js
@@ -6,12 +6,16 @@ License: MIT.
 Contac me in: asdrubalcorales92@gmail.com
 My phone number is: +58 412 4811678 */
 
-//Var:
+//Var and Hoisting:
 let serverExpress = require('http'); //serverExpress contains all the methods of the HTTP protocol to run a web server
 let url = require('url');//Records the URL address and allows you to specify custom behavior
 let fs = require('fs');//File system
 const color = require('colors');//Add color 
 let listenPort = 1111;//listen virtual port default 
+let index;//Hoisting(declarar sin asignar valor para apartar un espacio de memoría)
+let container;
+let registration;
+let Rute;
 
 console.log('\n==================𝙉𝙤𝙙𝙚 𝙀𝙭𝙥𝙧𝙚𝙨𝙨 𝙎𝙚𝙧𝙫𝙚𝙧===========')
 console.log('                    ░░░░░░░░▄▄█▄▄')
@@ -26,17 +30,19 @@ function Init(Router,manejador)//Capsule
     function startServerExpress(requires,reply)//This function starts Node Express Server
     {
         //Router:
-        let Rute = url.parse(requires.url).pathname;//captures what is entered in the url and stores it in a variable
-        let registration = fs.createWriteStream('registration.txt',{'flags':'a'});//flags. add information registration.txt
+        Rute = url.parse(requires.url).pathname;//captures what is entered in the url and stores it in a variable
+        registration = fs.createWriteStream('registration.txt',{'flags':'a'});//flags. add information registration.txt
         console.log('\tA new connection detected'.inverse+'\n');
-        let container = Router(manejador,Rute,reply);//Call function router in roterNodExpresserver.js
+        //container = Router(manejador,Rute,reply);//Call function router in roterNodExpresserver.js
+        //let container = Router(manejador,Rute);//Call function router in roterNodExpresserver.js
         registration.write(Rute+'\n');// Conection Log
-        
+        index = fs.readFileSync("www/index.html");//Read file from hard drive:
         //reply.writeHead(codeBrowser,{"Content-Type":"text/html"});//whriteHead sends the type of document to the header to the borwser
         //reply.write("<br><center><h1>Welcome to Node Express Server</h1><br>Powered by .::CORALESoftware::.</center>");
         //reply.write("<br><br>   "+Rute+" is a new connection, detected in: "+container)
         //reply.write("<br><br>   A new rute detected... "+Rute)
-        //reply.end(); // This line closes the connection
+        reply.write(index);
+        reply.end(); // This line closes the connection
         console.log('--------------------------------------------------'.rainbow)
     }
     serverExpress.createServer(startServerExpress).listen(listenPort);//Call the Node Express Server
