24e70cede651bfee750129d988072f6a666f3728
Asdrubal Corales <skorpico@gmail.com>
2023-02-02 18:20:38 -0400

    Modificación de cmentarios y código

--- requestsNodExpresserver.js
+++ requestsNodExpresserver.js
@@ -2,6 +2,10 @@
 Name of the proyect: 𝙉𝙤𝙙𝙚 𝙀𝙭𝙥𝙧𝙚𝙨𝙨 𝙎𝙚𝙧𝙫𝙚𝙧
 Name of the archive: requestsNodExpresserver*/
 
+    //lock Code for functions
+    //let now = new Date().getTime;
+    //while(new Date().getTime < now + 2000)
+
 //Var and Hoisting:
 //let fs = require('fs');//File system
 //let index;
@@ -9,46 +13,39 @@ Name of the archive: requestsNodExpresserver*/
 //Funtions:
 function xinit(xreply)
 {
-    console.log('* Detection in the Home Page Node Express Server');
-    //if(Rute == '/'){Rute = "index.html";}
-    //index = fs.readFileSync("www/"+Rute+".html");//Read file from hard drive:
-    //xreply.write(index);
-    
-    //lock Code for xinit
-    //let now = new Date().getTime;
-    //while(new Date().getTime < now + 2000)
-   //xreply.writeHead(codeBrowser,{"Content-Type":"text/html"});
-   //xreply.write("<br><center><h1>Welcome to Node Express Server</h1><br>Powered by .::CORALESoftware::.</center>");
+    console.log('* '+'Detection path'.cyan.underline+' in home page');
+    //xreply.writeHead(codeBrowser,{"Content-Type":"text/html"});
+    //xreply.write("<br><center><h1>Welcome to Node Express Server</h1><br>Powered by .::CORALESoftware::.</center>");
     //xreply.write('<br></br><p>This is Home Page and Node Express Server description</p>');
     //reply.write("<br><br>   "+Rute+" is a new connection, detected in: "+container)
     //reply.write("<br><br>   A new rute detected... "+Rute)
-    //xreply.end('End of connection');    
+    //xreply.end('End of the connection');    
     return 'xinit';
 }
 
 function information(xreply)
 {
-    console.log('* Detection in the information Node Express Server');
+    console.log('* '+'Detection path'.cyan.underline+' in information page');
     //xreply.writeHead(codeBrowser,{"Content-Type":"text/html"});
     //xreply.write("<br><center><h1>Welcome to Node Express Server</h1><br>Powered by .::CORALESoftware::.</center>");
     //xreply.write('<br></br><p>This is first Page.</p>');
-    //xreply.end();  
+    //xreply.end('End of the connection');  
     return 'information';
 }
 
 function services(xreply)
 {
-    console.log('* Detection in the services page Node Express Server');
+    console.log('* '+'Detection path'.cyan.underline+' in services page');
     //xreply.writeHead(codeBrowser,{"Content-Type":"text/html"});
     //xreply.write("<br><center><h1>Welcome to Node Express Server</h1><br>Powered by .::CORALESoftware::.</center>");
     //xreply.write('<br></br><p>This is Services Page.</p>');
-    //xreply.end(); 
+    //xreply.end('End of the connection'); 
     return 'services';
 }
 
 function contact(xreply)
 {
-    console.log('* Detection in the contact page Node Express Server');
+    console.log('-> Detection path:'.bold.cyan+' in contact page');
     //xreply.writeHead(codeBrowser,{"Content-Type":"text/html"});
     //xreply.write("<br><center><h1>Welcome to Node Express Server</h1><br>Powered by .::CORALESoftware::.</center>");
     //xreply.write('<br></br><p>This is second Page.</p>');
@@ -58,13 +55,15 @@ function contact(xreply)
 
 function favicon(xreply)//Controller favicon error
 {
-    console.log('* Controlled '+'favicon bug'.italic.red.underline);
+    
+    console.log('-> Controlled: '.bold.red+'favicon bug');
+    console.warn('-> Warning: '.bold.yellow+'will be failure!');
     //xreply.writeHead(codeBrowser,{"Content-Type":"text/html"});
     //xreply.write("");
     //xreply.end("End of connection"); 
     return "";
 }
-//Modification exports
+//Add exports in a object
 module.exports =
 {
     xinit:xinit,
