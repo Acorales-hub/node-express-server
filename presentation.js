d1c02cb36c4c598df0d0c19ae44db154c697f290
Asdrubal Corales <skorpico@gmail.com>
2023-02-02 18:22:44 -0400

    Se crea presentation.js que contiene una función la cual solo muestra un encabezado de bienvenida por el terminal de cmdo

---
+++ presentation.js
@@ -0,0 +1,11 @@
+function show()
+{
+    //xmsg = 'Cath the msg ->'+msg;
+    console.log('\n==================𝙉𝙤𝙙𝙚 𝙀𝙭𝙥𝙧𝙚𝙨𝙨 𝙎𝙚𝙧𝙫𝙚𝙧===========')
+    console.log('                    ░░░░░░░░▄▄█▄▄')
+    console.log('By Ing. A Corales P.░░░░▀▀▀██▀▀▀██▀▀▀')
+    console.log('    MIT LICESE      ▄▄▄▄▄▄▄███████▄▄▄▄▄▄▄')
+    console.log('                    ░░█▄█░░▀██▄██▀░░█▄█')
+    console.log('____________________________________________________')
+    //return xmsg;
+}module.exports={show:show};
