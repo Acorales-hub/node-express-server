6207209f65887ea3d2b060b3fa754355ec044674
Asdrubal Corales <skorpico@gmail.com>
2023-02-08 20:40:49 -0400

    Se agrego un valor al vector

--- initNodExpresserver.js
+++ initNodExpresserver.js
@@ -7,12 +7,14 @@ const Server = require('./node-express-server');//Call Server
 const Xrouter = require('./routerNodExpresserver');//Call router
 const Xrequests = require('./requestsNodExpresserver.js');//Call requests
 
-//Requests handler
+//Requests xHandler
 let manejador = {}//Vector
 manejador['/'] = Xrequests.xinit;
+manejador['index.html']=Xrequests.indexHtml;
 manejador['/information'] = Xrequests.information;
 manejador['/services'] = Xrequests.services;
 manejador['/contact'] = Xrequests.contact;
+manejador['/notFound'] = Xrequests.notFound;
 manejador['/favicon.ico'] = Xrequests.favicon;//Controller favicon error
 
 Server.Init(Xrouter.Router,manejador);//Call the init and router
